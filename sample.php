24ade5d988c354905ec714daada29784d1568490
neilbragado <57480053+neilbragado@users.noreply.github.com>
2020-03-10 12:06:05 +0800

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,3 +1,27 @@
 <?php
-	echo "Hello World!!!!!!!";
+	$servername = "neilit330server.mysql.database.azure.com";
+	$username = "NeilOliver@neilit330server";
+	$password = "Neilbragado18";
+	$dbname = "it330db";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	// Check connection
+	if (!$conn) {
+	    die("Connection failed: " . mysqli_connect_error());
+	}
+
+	$sql = "SELECT id, description, count FROM inventory";
+	$result = mysqli_query($conn, $sql);
+
+	if (mysqli_num_rows($result) > 0) {
+	    // output data of each row
+	    while($row = mysqli_fetch_assoc($result)) {
+		echo "id: " . $row["id"]. " - Product: " . $row["description"]. " -Count: " . $row["count"]. "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}
+
+	mysqli_close($conn);
 ?>
